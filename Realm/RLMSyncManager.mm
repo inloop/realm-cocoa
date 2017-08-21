9258a500ca01504bf005ffe84f9199a23f67914b
Austin Zheng <austinzheng@gmail.com>
2017-08-21 16:22:48 -0700

    Make object store get the bundle identifier

--- Realm/RLMSyncManager.mm
+++ Realm/RLMSyncManager.mm
@@ -109,11 +109,7 @@ static dispatch_once_t s_onceToken;
         bool should_encrypt = !getenv("REALM_DISABLE_METADATA_ENCRYPTION") && !RLMIsRunningInPlayground();
         auto mode = should_encrypt ? SyncManager::MetadataMode::Encryption : SyncManager::MetadataMode::NoEncryption;
         rootDirectory = rootDirectory ?: [NSURL fileURLWithPath:RLMDefaultDirectoryForBundleIdentifier(nil)];
-        util::Optional<std::string> service;
-        if (auto bundleIdentifier = [[NSBundle mainBundle] bundleIdentifier]) {
-            service = util::make_optional<std::string>([bundleIdentifier UTF8String]);
-        }
-        SyncManager::shared().configure_file_system(rootDirectory.path.UTF8String, mode, none, true, service);
+        SyncManager::shared().configure_file_system(rootDirectory.path.UTF8String, mode, none, true);
         return self;
     }
     return nil;
